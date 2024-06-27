3ee114e464e504efac8199c0f3c0d3e29cff454b
Miguel Primera <primerariosm@gmail.com>
2024-06-27 11:15:44 -0500

    Archivo Index

---
+++ main.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const temp = document.querySelector("#temperatura");
+    const btn = document.querySelector("#btnAction");
+    const result = document.querySelector("#resultCelsius");
+    
+    btn.addEventListener("click", (event) => {
+        event.preventDefault()
+
+        const tempValue = parseFloat(temp.value);
+
+        if (isNaN(tempValue)) {
+            result.innerHTML = "Por favor, ingrese un número válido.";
+            return;
+        }
+
+        const resultado = (tempValue - 32) * 5 / 9; // Correct conversion from Fahrenheit to Celsius
+        result.innerHTML = `${resultado.toFixed(2)} °C`;
+    
+    });
+})
